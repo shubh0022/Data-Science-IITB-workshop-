09b03a948b31053d95bca55bd6c691c029c6d582
YADAV SHUBHAM JANGBAHADUR <153888821+shubh0022@users.noreply.github.com>
2024-12-25 12:16:49 +0530

    Create IITB 18 DEC pythan.py

---
+++ IITB 18 DEC pythan.py
@@ -0,0 +1,47 @@
+#'''' what is the data type '''
+a= "Hello, World!"
+print(type(a))
+
+
+
+#''' prnt thr value  ( also single or doubelequot is  mention the string) '''
+print("Hello, World!", 'aaa')
+
+
+#'''addition'''
+a=20
+b=3
+print (a+b)
+
+
+#''' upper case '''
+
+a= "Hello, World!"
+print(a.upper())
+
+#'''boolen''
+
+a,b=True, False
+print(type (a))
+
+
+
+#'''inter changing the data typ'''
+
+a=2.3
+print(int(a))
+ 
+ 
+ # slicing or diasing
+#list
+
+a=['z', 'xxy' , 2, True]
+sliced_string =a[:1]
+
+print (sliced_string)
+
+
+#questions [2,61]
+#a[23,47,'abc',True,'xyz']
+#print(a[4:-6:-4])
+
